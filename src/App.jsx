ad57ed4e59b220f4131827ae576caabcf29446d9
charles <codelyfewithcharlie@gmail.com>
2019-10-13 14:44:50 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,31 @@ import Person from "./components/Person/Person";
 import "./App.scss";
 
 class App extends React.Component {
+	state = {
+		persons: [{ name: "Max" }, { name: "Charlie" }, { name: "Sean" }]
+	};
+
+	switchNameHandler = () => {
+		this.setState({
+			persons: [
+				{ name: "Super Max" },
+				{ name: "Monster Charlie" },
+				{ name: "Lil Sean" }
+			]
+		});
+	};
+
 	render() {
-		return <Person />;
+		return (
+			<div>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person name={this.state.persons[0].name} />
+				<Person name={this.state.persons[1].name}>
+					My Hobbies: Computer Stuff
+				</Person>
+				<Person name={this.state.persons[2].name} />
+			</div>
+		);
 	}
 }
 export default App;
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Person = () => {
-	return <div>Im a person</div>;
+const Person = props => {
+	return (
+		<div>
+			<p>
+				My name is {props.name} and I am {Math.floor(Math.random() * 30)} years
+				old.
+			</p>
+			<p>{props.children}</p>
+		</div>
+	);
 };
 
 export default Person;
